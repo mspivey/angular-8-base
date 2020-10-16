9e6de831a374ce7cc44cc28da5dc1429fbe7f507
mspivey <markspivey73@googlemail.com>
2020-10-16 07:47:18 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+}
